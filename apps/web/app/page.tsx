b049bcdade27279556b4c24d9735e406439919c7
SUPAWIT MARAYAT <supawit.mara@kmutt.ac.th>
2025-08-08 13:13:13 +0000

    Feat/preset typography

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,18 +9,34 @@ import Story from "@/app/_components/story"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-20">
+    <div className="no-scrollbar h-screen snap-y snap-always overflow-y-auto bg-black">
       {/* spacec for navbar */}
       <div className="h-[94.45px]" />
 
-      <LandingSection />
-      <QualificationSector />
-      <Application />
-      <Award />
-      <DateAndContest />
-      <Scope />
-      <Contact />
-      <Story />
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <LandingSection />
+      </div>
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <QualificationSector />
+      </div>
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <Application />
+      </div>
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <Award />
+      </div>
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <DateAndContest />
+      </div>
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <Scope />
+      </div>
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <Contact />
+      </div>
+      <div className="flex min-h-screen snap-start items-center justify-center">
+        <Story />
+      </div>
     </div>
   )
 }
