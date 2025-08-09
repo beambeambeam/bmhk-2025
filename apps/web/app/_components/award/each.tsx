c87148780ae0d6ed3e3c129b87832c7455f27f4a
JAKKAPHAT CHALERMPHANAPHAN <jakkaphat.chal@kmutt.ac.th>
2025-08-09 23:21:10 +0700

    fix: correct width for first place

--- apps/web/app/_components/award/each.tsx
+++ apps/web/app/_components/award/each.tsx
@@ -15,7 +15,7 @@ export function EachAwardForTopRow({ data }: EachAwardProps) {
           alt={data.title}
         />
         <div className="flex flex-col gap-6">
-          <div className="inline-flex flex-col items-center justify-center gap-10 rounded-[40px] px-8 py-6 outline-1 outline-offset-[-1px]">
+          <div className="mx-auto inline-flex w-fit flex-col items-center justify-center gap-10 rounded-[40px] px-8 py-6 outline-1 outline-offset-[-1px]">
             <div className="text-subheader-2 justify-center self-stretch text-center">{data.title}</div>
           </div>
           <div className="text-body-3 justify-center text-center">{data.description}</div>
