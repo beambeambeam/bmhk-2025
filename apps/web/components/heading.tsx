74c513039f33f1536de254fc4cc4437f1b3f9380
JAKKAPHAT CHALERMPHANAPHAN <jakkaphat.chal@kmutt.ac.th>
2025-08-10 17:11:44 +0700

    feat: responsive size for heading component

--- apps/web/components/heading.tsx
+++ apps/web/components/heading.tsx
@@ -5,12 +5,12 @@ interface HeadingProps extends React.HTMLAttributes<HTMLDivElement> {
 export function Heading({ text, ...props }: HeadingProps) {
   return (
     <div {...props}>
-      <div className="flex w-full items-center justify-center gap-8">
-        <div className="h-[2px] w-[360px] rounded-full bg-gradient-to-l from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent" />
-        <div className="text-shadow-[0_0_5px_#FFFFFF] text-center text-5xl font-medium leading-[140%] text-white">
+      <div className="flex w-full items-center justify-center gap-4 md:gap-8">
+        <div className="w-17 h-0 rounded-full bg-gradient-to-l from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent md:h-[1px] md:w-[338px] lg:h-[2px] lg:w-[360px]" />
+        <div className="text-shadow-[0_0_5px_#FFFFFF] text-center text-2xl font-medium leading-loose text-white md:text-4xl md:leading-[56px] lg:text-5xl lg:leading-[140%]">
           {text}
         </div>
-        <div className="h-[2px] w-[360px] rounded-full bg-gradient-to-r from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent" />
+        <div className="w-17 h-0 rounded-full bg-gradient-to-r from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent md:h-[1px] md:w-[338px] lg:h-[2px] lg:w-[360px]" />
       </div>
     </div>
   )
