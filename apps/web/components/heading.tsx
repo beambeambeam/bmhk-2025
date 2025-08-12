fefec8d5e4bba95ebb6004fbe94319fe83403f92
Feen Phoorin <118984693+2005fpp@users.noreply.github.com>
2025-08-12 20:34:54 +0700

    ✨Feat/dateandcontest (#10)
    * feat: add lqGlass class for dateandcontest
    
    * feat: add dateandcontest page
    
    * feat: add responsive (lg, 2xl) - not done

--- apps/web/components/heading.tsx
+++ apps/web/components/heading.tsx
@@ -5,12 +5,12 @@ interface HeadingProps extends React.HTMLAttributes<HTMLDivElement> {
 export function Heading({ text, ...props }: HeadingProps) {
   return (
     <div {...props}>
-      <div className="flex w-full items-center justify-center gap-4 md:gap-8">
-        <div className="w-17 h-[1px] rounded-full bg-gradient-to-l from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent md:w-[338px] lg:h-[2px] lg:w-[360px]" />
-        <div className="text-shadow-[0_0_5px_#FFFFFF] text-center text-2xl font-medium leading-loose text-white md:text-4xl md:leading-[56px] lg:text-5xl lg:leading-[140%]">
+      <div className="flex w-full items-center justify-center gap-4 lg:gap-8">
+        <div className="h-[1px] w-[83.5px] rounded-full bg-gradient-to-l from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent lg:w-[376.5px] 2xl:w-[361px]" />
+        <div className="text-shadow-[0_0_5px_#FFFFFF] whitespace-nowrap text-center text-2xl font-medium text-white lg:text-[2.5rem] 2xl:text-5xl">
           {text}
         </div>
-        <div className="w-17 h-[1px] rounded-full bg-gradient-to-r from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent md:w-[338px] lg:h-[2px] lg:w-[360px]" />
+        <div className="h-[1px] w-[83.5px] rounded-full bg-gradient-to-r from-[#FFFFFF] via-[#C9DDFA61] via-[38%] to-transparent lg:w-[376.5px] 2xl:w-[361px]" />
       </div>
     </div>
   )
