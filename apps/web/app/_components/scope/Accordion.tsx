766d08b2f48cd82f993f47a20158592aeb7d540f
Feen Phoorin <118984693+2005fpp@users.noreply.github.com>
2025-08-13 00:56:20 +0700

    ✨ feat: scope section (#9)
    * feat: add lqGlass class for scope to global.css
    
    * feat: add scope page
    
    * feat: add responsive (lg, 2xl)
    
    ---------
    
    Co-authored-by: Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>

---
+++ apps/web/app/_components/scope/Accordion.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import React, { useState } from "react"
+
+import AccordionItem from "./AccordionItem"
+import { contentData } from "./content"
+import { AccordionProps } from "./types"
+
+export const Accordion: React.FC<AccordionProps> = ({ type = "single", collapsible = true }) => {
+  const [openItem, setOpenItem] = useState<string | null>(null)
+  const [openItems, setOpenItems] = useState<string[]>([])
+
+  const toggleItem = (id: string) => {
+    if (type === "single") {
+      setOpenItem((prev) => (prev === id && collapsible ? null : id))
+    } else {
+      setOpenItems((prev) => (prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]))
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-y-6 lg:gap-y-8 2xl:gap-y-10">
+      {contentData.map((data) => {
+        const isOpen = type === "single" ? openItem === data.id : openItems.includes(data.id)
+        return (
+          <AccordionItem
+            key={data.id}
+            value={data.id}
+            isOpen={isOpen}
+            onToggle={toggleItem}
+            trigger={data.title}
+            lqClassName={data.lqClassName}
+            colorTheme={data.colorTheme}
+            data={data}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default Accordion
