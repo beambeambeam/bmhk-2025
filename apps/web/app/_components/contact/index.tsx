42d0327d54b0a87fcb20014e33c9ae3eb9c2e782
Rangsiman Jerabunjerdchai <61783407+RangsimanJera@users.noreply.github.com>
2025-08-12 20:56:02 +0700

    Contact Story Footer (#12)
    * Contact Story Footer
    
    * fix: case sensitive filename for Star_bright
    
    * chore: fix broken lockfile
    
    ---------
    
    Co-authored-by: JAKKAPHAT CHALERMPHANAPHAN <gunt@guntxjakka.me>

--- apps/web/app/_components/contact/index.tsx
+++ apps/web/app/_components/contact/index.tsx
@@ -1,4 +1,83 @@
+import { Heading } from '@/components/heading';
+import IconCircle from '@/components/iconCircle';
+import MapEmbed from '@/components/mapEmbed';
+
 function Contact() {
-  return <div className="h-[1080px] w-full">Contact</div>
+  return (
+    <div className="w-full px-6 flex flex-col items-center justify-center content-center gap-8 lg:max-h-[834] sm:px-20 lg:gap-10 2xl:max-h-[1080] 2xl:px-40 2xl:gap-15">
+      <Heading text="ติดต่อทีมงาน" />
+      <div className='flex gap-10 w-full 2xl:max-w-[1326]'>
+        <div className='flex flex-row justify-center gap-8 2xl:gap-10 w-full'>
+          <div className='flex flex-1 flex-col gap-6 lg:gap-8 2xl:gap-10'> {/* Left Section */}
+            <div className='flex flex-col gap-6 lg:gap-8 md:flex-row'>
+              <div className='flex flex-col gap-4 flex-1 min-w-0'> {/* Contact */}
+                <div className='flex gap-4 items-center'>
+                  <IconCircle className='w-[50px] h-[50px] 2xl:w-[60px] 2xl:h-[60px] flex-shrink-0'>
+                    <img
+                      src="/static/icon/Basic.svg"
+                      alt="Staff Icon"
+                      className='w-[30px] h-[30px] 2xl:w-9 2xl:h-9'
+                    />
+                  </IconCircle>
+                  <div className='text-[var(--color-gray-50)] text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>คำถามทั่วไป</div>
+                </div>
+                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>ชญานิษฐ์ กันตนฤมิตรกุล (ชิชา)</div>
+                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>099 197 9119</div>
+                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px] break-words whitespace-normal'>chayanit.kunt@mail.kmutt.ac.th</div>
+              </div>
+              <div className='flex flex-col gap-4 flex-1 min-w-0'> {/* Contact */}
+                <div className='flex gap-4 items-center'>
+                  <IconCircle className='w-[50px] h-[50px] 2xl:w-[60px] 2xl:h-[60px] flex-shrink-0'>
+                    <img
+                      src="/static/icon/Code.svg"
+                      alt="Staff Icon"
+                      className='w-[30px] h-[30px] 2xl:w-9 2xl:h-9'
+                    />
+                  </IconCircle>
+                  <div className='text-[var(--color-gray-50)] text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>คำถามด้านวิชาการ</div>
+                </div>
+                <div className='text-body-1 text-[16px] lg:text-[20px] 2xl:text-[24px]'>ชญานิษฐ์ กันตนฤมิตรกุล (ชิชา)</div>
+                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>000 000 0000</div>
+                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px] break-words whitespace-normal'>cpe@kmutt.ac.th</div>
+              </div>
+            </div>
+            <div className='flex flex-col gap-4'> {/* Contact */}
+              <div className='flex gap-4 items-center'>
+                <IconCircle className='w-[50px] h-[50px] 2xl:w-[60px] 2xl:h-[60px] flex-shrink-0'>
+                  <img
+                    src="/static/icon/Address.svg"
+                    alt="Address Icon"
+                    className='w-[30px] h-[30px] 2xl:w-9 2xl:h-9'
+                  />
+                </IconCircle>
+                <div className='text-[var(--color-gray-50)] text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์</div>
+              </div>
+              <div className='text-body-1 text-[16px] lg:text-[20px] 2xl:text-[24px]'>อาคารวิศววัฒนะ ชั้น 10 เลขที่ 126 ถ.ประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140</div>
+            </div>
+            <div className='flex gap-6'>
+              <a href="https://www.facebook.com/cpe.kmutt" target="_blank" rel="noopener noreferrer">
+                <IconCircle className='w-[60px] h-[60px] lg:w-[70px] lg:h-[70px] 2xl:w-[90px] 2xl:h-[90px] flex-shrink-0'>
+                  <img
+                    src="/static/icon/Facebook.svg"
+                    alt="Facebook Icon"
+                  />
+                </IconCircle>
+              </a>
+              <a href="https://www.instagram.com/cpe_studentunion" target="_blank" rel="noopener noreferrer">
+                <IconCircle className='w-[60px] h-[60px] lg:w-[70px] lg:h-[70px] 2xl:w-[90px] 2xl:h-[90px] flex-shrink-0'>
+                  <img
+                    src="/static/icon/Instagram.svg"
+                    alt="Instagram Icon"
+                  />
+                </IconCircle>
+              </a>
+            </div>
+          </div>
+          <MapEmbed className='flex-shrink-0 hidden lg:block md:w-[384px] 2xl:w-[506px]' /> {/* Right Section */}
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Contact
+
+export default Contact;
---
+++ apps/web/app/_components/footer.tsx
@@ -0,0 +1,156 @@
+'use client';
+
+import GlassCard from "@/components/glassCard";
+import IconCircle from "@/components/iconCircle";
+
+import { useRouter } from 'next/navigation';
+
+function Footer() {
+  const router = useRouter();
+
+  return (
+    <GlassCard
+      className="p-6 lg:py-15 lg:px-20 2xl:py-[90px] 2xl:px-[120px]"
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 40,
+        alignSelf: 'stretch',
+        borderRadius: '60px 60px 0 0',
+        background: `
+          radial-gradient(
+            66.31% 84.48% at 52.63% -21.84%, 
+            rgba(255, 204, 247, 0.5) 0%, 
+            rgba(2, 6, 3, 0) 100%
+          ),
+          linear-gradient(
+            -106.52deg, 
+            rgba(255, 204, 247, 0.09) -2.48%, 
+            rgba(159, 131, 220, 0.09) 29.08%
+          )
+        `,
+        borderTop: 'none',
+        borderLeft: 'none',
+      }}
+    >
+      <div className="flex flex-col pb-10 justify-center items-center gap-10 self-stretch border-b-[0.5px] border-solid border-[#666]">
+        <div className="flex flex-col items-start gap-10 self-stretch 2xl:flex-row">
+          <div className="flex flex-col items-start gap-10 flex-[1_0_0]">
+            <div className="hidden lg:flex justify-between items-center w-full">
+              <div className="flex h-[70px] 2xl:h-[75px] items-center gap-10"> {/* Icon */}
+                <img
+                  src="/static/logo/Logo.png"
+                  alt="Bangmod Hackathon Logo"
+                  className="max-w-[220px] max-h-[72px] object-contain"
+                />
+                <img
+                  src="/static/logo/CPE.png"
+                  alt="CPE Logo"
+                  className="max-w-[220px] max-h-[72px] object-contain"
+                />
+                <img
+                  src="/static/logo/KMUTT.png"
+                  alt="KMUTT Logo"
+                  className="max-w-[220px] max-h-[72px] object-contain"
+                />
+              </div>
+              <div className="flex items-center gap-6 2xl:hidden">  {/* Social Media */}
+                <a href="https://www.facebook.com/cpe.kmutt" target="_blank" rel="noopener noreferrer">
+                  <IconCircle className='w-[60px] h-[60px] lg:w-[70px] lg:h-[70px] 2xl:w-[80px] 2xl:h-[80px] flex-shrink-0'>
+                    <img
+                      src="/static/icon/Facebook.svg"
+                      alt="Facebook Icon"
+                    />
+                  </IconCircle>
+                </a>
+                <a href="https://www.instagram.com/cpe_studentunion" target="_blank" rel="noopener noreferrer">
+                  <IconCircle className='w-[60px] h-[60px] lg:w-[70px] lg:h-[70px] 2xl:w-[80px] 2xl:h-[80px] flex-shrink-0'>
+                    <img
+                      src="/static/icon/Instagram.svg"
+                      alt="Instagram Icon"
+                    />
+                  </IconCircle>
+                </a>
+              </div>
+            </div>
+            <div className="flex lg:hidden flex-col justify-center items-center gap-6 w-full">   {/* Logo Mobile */}
+              <img
+                  src="/static/logo/Logo.png"
+                  alt="Bangmod Hackathon Logo"
+                  className="max-w-[220px] h-[60px] object-contain"
+                />
+                <div className="flex h-10 gap-4">
+                  <img
+                    src="/static/logo/CPE.png"
+                    alt="CPE Logo"
+                    className="max-w-[220px] max-h-[72px] object-contain"
+                    />
+                  <img
+                    src="/static/logo/KMUTT.png"
+                    alt="KMUTT Logo"
+                    className="max-w-[220px] max-h-[72px] object-contain"
+                  />
+                </div>
+            </div>
+            <div className="flex flex-col items-start gap-6 self-stretch max-lg:items-center">
+              <div className="text-body-1 max-lg:text-[18px] max-2xl:text-[20px] text-[var(--color-gray-50)] max-lg:text-center">การแข่งขัน เขียนโปรแกรมคอมพิวเตอร์ บางมดแฮกกาธอน</div>
+              <div className="flex flex-col items-start gap-3 self-stretch">
+                <div className="text-body-3 max-lg:text-[14px] text-[var(--color-gray-100)] max-lg:text-center">ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี อาคารวิศววัฒนะ ชั้น 10 เลขที่ 126 ถ.ประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140</div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-end gap-10 flex-[1_0_0] self-stretch"> {/* Sponsor */}
+            <div className="flex flex-col items-start gap-7 flex-[1_0_0] self-stretch max-lg:items-center">
+              <div className="text-body-1 max-lg:text-[18px] max-2xl:text-[20px] text-[var(--color-gray-50)]">สนับสนุนโดย</div>
+              <div className="flex flex-wrap items-center gap-6 self-stretch max-lg:justify-center">
+                <GlassCard className="w-15 h-15 2xl:w-20 2xl:h-20" style={{ borderRadius: 24, border: "1.5px solid rgba(255, 255, 255, 0.10)" }} />
+                <GlassCard className="w-15 h-15 2xl:w-20 2xl:h-20" style={{ borderRadius: 24, border: "1.5px solid rgba(255, 255, 255, 0.10)" }} />
+                <GlassCard className="w-15 h-15 2xl:w-20 2xl:h-20" style={{ borderRadius: 24, border: "1.5px solid rgba(255, 255, 255, 0.10)" }} />
+                <GlassCard className="w-15 h-15 2xl:w-20 2xl:h-20" style={{ borderRadius: 24, border: "1.5px solid rgba(255, 255, 255, 0.10)" }} />
+                <GlassCard className="w-15 h-15 2xl:w-20 2xl:h-20" style={{ borderRadius: 24, border: "1.5px solid rgba(255, 255, 255, 0.10)" }} />
+                <GlassCard className="w-15 h-15 2xl:w-20 2xl:h-20" style={{ borderRadius: 24, border: "1.5px solid rgba(255, 255, 255, 0.10)" }} />
+              </div>
+              <div className="hidden 2xl:flex flex-col items-start gap-3 self-stretch">
+                <div className="text-body-3 text-[var(--color-gray-50)]">เราขอขอบคุณ สปอนเซอร์ 1, สปอนเซอร์ 2, สปอนเซอร์ 3, สปอนเซอร์ 4, สปอนเซอร์ 5, 
+สปอนเซอร์ 6</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center self-stretch max-lg:justify-center">
+          <div className="flex lg:hidden 2xl:flex items-center gap-6">
+            <a href="https://www.facebook.com/cpe.kmutt" target="_blank" rel="noopener noreferrer">
+              <IconCircle className='w-[60px] h-[60px] 2xl:w-[80px] 2xl:h-[80px] flex-shrink-0'>
+                <img
+                  src="/static/icon/Facebook.svg"
+                  alt="Facebook Icon"
+                />
+              </IconCircle>
+            </a>
+            <a href="https://www.instagram.com/cpe_studentunion" target="_blank" rel="noopener noreferrer">
+              <IconCircle className='w-[60px] h-[60px] 2xl:w-[80px] 2xl:h-[80px] flex-shrink-0'>
+                <img
+                  src="/static/icon/Instagram.svg"
+                  alt="Instagram Icon"
+                />
+              </IconCircle>
+            </a>
+          </div>
+          <div className="hidden lg:flex lg:w-full 2xl:w-[820px] justify-between items-center">
+            <div className="text-nav-2" onClick={() => router.push('/landing')}>รายละเอียด</div>
+            <div className="text-nav-2" onClick={() => router.push('/qualification')}>คุณสมบัติ</div>
+            <div className="text-nav-2" onClick={() => router.push('/award')}>รางวัล</div>
+            <div className="text-nav-2" onClick={() => router.push('/dateandcontest')}>กำหนดการ</div>
+            <div className="text-nav-2" onClick={() => router.push('/contact')}>ติดต่อทีมงาน</div>
+            <div className="text-nav-2" onClick={() => router.push('/')}>นโยบายความเป็นส่วนตัว</div>
+          </div>
+        </div>
+      </div>
+      <div className="text-body-3 text-[16px] max-lg:text-[10px] text-center">© 2025 Bangmod Hackathon, Department of Computer Engineering. All rights reserved.</div>
+    </GlassCard>
+  );
+}
+
+export default Footer;
