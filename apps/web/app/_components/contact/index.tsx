6dadbe322eb77a9bf338598785ae1aeb534dc25f
JAKKAPHAT CHALERMPHANAPHAN <gunt@guntxjakka.me>
2025-08-16 06:44:29 +0700

    fix: update contact info

--- apps/web/app/_components/contact/index.tsx
+++ apps/web/app/_components/contact/index.tsx
@@ -1,83 +1,102 @@
-import { Heading } from '@/components/heading';
-import IconCircle from '@/components/iconCircle';
-import MapEmbed from '@/components/mapEmbed';
+import { Heading } from "@/components/heading"
+import IconCircle from "@/components/iconCircle"
+import MapEmbed from "@/components/mapEmbed"
 
 function Contact() {
   return (
-    <div className="w-full px-6 flex flex-col items-center justify-center content-center gap-8 lg:max-h-[834] sm:px-20 lg:gap-10 2xl:max-h-[1080] 2xl:px-40 2xl:gap-15">
+    <div className="2xl:gap-15 flex w-full flex-col content-center items-center justify-center gap-8 px-6 sm:px-20 lg:max-h-[834] lg:gap-10 2xl:max-h-[1080] 2xl:px-40">
       <Heading text="ติดต่อทีมงาน" />
-      <div className='flex gap-10 w-full 2xl:max-w-[1326]'>
-        <div className='flex flex-row justify-center gap-8 2xl:gap-10 w-full'>
-          <div className='flex flex-1 flex-col gap-6 lg:gap-8 2xl:gap-10'> {/* Left Section */}
-            <div className='flex flex-col gap-6 lg:gap-8 md:flex-row'>
-              <div className='flex flex-col gap-4 flex-1 min-w-0'> {/* Contact */}
-                <div className='flex gap-4 items-center'>
-                  <IconCircle className='w-[50px] h-[50px] 2xl:w-[60px] 2xl:h-[60px] flex-shrink-0'>
+      <div className="flex w-full gap-10 2xl:max-w-[1326]">
+        <div className="flex w-full flex-row justify-center gap-8 2xl:gap-10">
+          <div className="flex flex-1 flex-col gap-6 lg:gap-8 2xl:gap-10">
+            {" "}
+            {/* Left Section */}
+            <div className="flex flex-col gap-6 md:flex-row lg:gap-8">
+              <div className="flex min-w-0 flex-1 flex-col gap-4">
+                {" "}
+                {/* Contact */}
+                <div className="flex items-center gap-4">
+                  <IconCircle className="h-[50px] w-[50px] flex-shrink-0 2xl:h-[60px] 2xl:w-[60px]">
                     <img
                       src="/static/icon/Basic.svg"
                       alt="Staff Icon"
-                      className='w-[30px] h-[30px] 2xl:w-9 2xl:h-9'
+                      className="h-[30px] w-[30px] 2xl:h-9 2xl:w-9"
                     />
                   </IconCircle>
-                  <div className='text-[var(--color-gray-50)] text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>คำถามทั่วไป</div>
+                  <div className="text-body-1 text-[18px] text-[var(--color-gray-50)] lg:text-[20px] 2xl:text-[24px]">
+                    คำถามทั่วไป
+                  </div>
                 </div>
-                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>ชญานิษฐ์ กันตนฤมิตรกุล (ชิชา)</div>
-                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>099 197 9119</div>
-                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px] break-words whitespace-normal'>chayanit.kunt@mail.kmutt.ac.th</div>
+                <div className="text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]">
+                  ชญานิษฐ์ กันตนฤมิตรกุล (ชิชา)
+                </div>
+                <div className="text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]">099 197 9119</div>
+                <div className="text-body-1 whitespace-normal break-words text-[18px] lg:text-[20px] 2xl:text-[24px]">
+                  chayanit.kunt@mail.kmutt.ac.th
+                </div>
               </div>
-              <div className='flex flex-col gap-4 flex-1 min-w-0'> {/* Contact */}
-                <div className='flex gap-4 items-center'>
-                  <IconCircle className='w-[50px] h-[50px] 2xl:w-[60px] 2xl:h-[60px] flex-shrink-0'>
+              <div className="flex min-w-0 flex-1 flex-col gap-4">
+                {" "}
+                {/* Contact */}
+                <div className="flex items-center gap-4">
+                  <IconCircle className="h-[50px] w-[50px] flex-shrink-0 2xl:h-[60px] 2xl:w-[60px]">
                     <img
                       src="/static/icon/Code.svg"
                       alt="Staff Icon"
-                      className='w-[30px] h-[30px] 2xl:w-9 2xl:h-9'
+                      className="h-[30px] w-[30px] 2xl:h-9 2xl:w-9"
                     />
                   </IconCircle>
-                  <div className='text-[var(--color-gray-50)] text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>คำถามด้านวิชาการ</div>
+                  <div className="text-body-1 text-[18px] text-[var(--color-gray-50)] lg:text-[20px] 2xl:text-[24px]">
+                    คำถามด้านวิชาการ
+                  </div>
                 </div>
-                <div className='text-body-1 text-[16px] lg:text-[20px] 2xl:text-[24px]'>ชญานิษฐ์ กันตนฤมิตรกุล (ชิชา)</div>
-                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>000 000 0000</div>
-                <div className='text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px] break-words whitespace-normal'>cpe@kmutt.ac.th</div>
+                <div className="text-body-1 text-[16px] lg:text-[20px] 2xl:text-[24px]">
+                  ภูริณัฐ พลอาสา (นาโน)
+                </div>
+                <div className="text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]">081 837 0772</div>
+                <div className="text-body-1 whitespace-normal break-words text-[18px] lg:text-[20px] 2xl:text-[24px]">
+                  phurinat.pola@kmutt.ac.th
+                </div>
               </div>
             </div>
-            <div className='flex flex-col gap-4'> {/* Contact */}
-              <div className='flex gap-4 items-center'>
-                <IconCircle className='w-[50px] h-[50px] 2xl:w-[60px] 2xl:h-[60px] flex-shrink-0'>
+            <div className="flex flex-col gap-4">
+              {" "}
+              {/* Contact */}
+              <div className="flex items-center gap-4">
+                <IconCircle className="h-[50px] w-[50px] flex-shrink-0 2xl:h-[60px] 2xl:w-[60px]">
                   <img
                     src="/static/icon/Address.svg"
                     alt="Address Icon"
-                    className='w-[30px] h-[30px] 2xl:w-9 2xl:h-9'
+                    className="h-[30px] w-[30px] 2xl:h-9 2xl:w-9"
                   />
                 </IconCircle>
-                <div className='text-[var(--color-gray-50)] text-body-1 text-[18px] lg:text-[20px] 2xl:text-[24px]'>ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์</div>
+                <div className="text-body-1 text-[18px] text-[var(--color-gray-50)] lg:text-[20px] 2xl:text-[24px]">
+                  ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์
+                </div>
               </div>
-              <div className='text-body-1 text-[16px] lg:text-[20px] 2xl:text-[24px]'>อาคารวิศววัฒนะ ชั้น 10 เลขที่ 126 ถ.ประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140</div>
+              <div className="text-body-1 text-[16px] lg:text-[20px] 2xl:text-[24px]">
+                อาคารวิศววัฒนะ ชั้น 10 เลขที่ 126 ถ.ประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140
+              </div>
             </div>
-            <div className='flex gap-6'>
-              <a href="https://www.facebook.com/cpe.kmutt" target="_blank" rel="noopener noreferrer">
-                <IconCircle className='w-[60px] h-[60px] lg:w-[70px] lg:h-[70px] 2xl:w-[90px] 2xl:h-[90px] flex-shrink-0'>
-                  <img
-                    src="/static/icon/Facebook.svg"
-                    alt="Facebook Icon"
-                  />
+            <div className="flex gap-6">
+              <a href="https://www.facebook.com/BangmodHackathon" target="_blank" rel="noopener noreferrer">
+                <IconCircle className="h-[60px] w-[60px] flex-shrink-0 lg:h-[70px] lg:w-[70px] 2xl:h-[90px] 2xl:w-[90px]">
+                  <img src="/static/icon/Facebook.svg" alt="Facebook Icon" />
                 </IconCircle>
               </a>
-              <a href="https://www.instagram.com/cpe_studentunion" target="_blank" rel="noopener noreferrer">
-                <IconCircle className='w-[60px] h-[60px] lg:w-[70px] lg:h-[70px] 2xl:w-[90px] 2xl:h-[90px] flex-shrink-0'>
-                  <img
-                    src="/static/icon/Instagram.svg"
-                    alt="Instagram Icon"
-                  />
+              <a href="https://www.instagram.com/bangmodhack.kmutt" target="_blank" rel="noopener noreferrer">
+                <IconCircle className="h-[60px] w-[60px] flex-shrink-0 lg:h-[70px] lg:w-[70px] 2xl:h-[90px] 2xl:w-[90px]">
+                  <img src="/static/icon/Instagram.svg" alt="Instagram Icon" />
                 </IconCircle>
               </a>
             </div>
           </div>
-          <MapEmbed className='flex-shrink-0 hidden lg:block md:w-[384px] 2xl:w-[506px]' /> {/* Right Section */}
+          <MapEmbed className="hidden flex-shrink-0 md:w-[384px] lg:block 2xl:w-[506px]" />{" "}
+          {/* Right Section */}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Contact;
+export default Contact
