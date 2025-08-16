75249cc2a69d907526951567569defcbd9e28a2e
Theeraphat Jaingam <jain.theera@gmail.com>
2025-08-16 17:38:09 +0700

    fix: adjust layout and styling for qualification section

--- apps/web/app/_components/qualification/index.tsx
+++ apps/web/app/_components/qualification/index.tsx
@@ -24,18 +24,20 @@ function QualificationSector() {
       <Heading text="คุณสมบัติผู้สมัคร" />
       <div className="flex flex-col items-center justify-center gap-6 md:flex-row md:gap-8 lg:gap-10">
         {qualificationData.map((item) => (
-          <div key={item.title} className="flex w-[415px] flex-col items-center justify-center gap-6">
+          <div
+            key={item.title}
+            className="flex w-[345px] flex-col items-center justify-center gap-4 md:w-[323.33px] md:gap-6 lg:w-[415px]">
             <div className="relative">
-              <div className="relative size-[330px] overflow-hidden rounded-full bg-[rgba(0,0,0,0.001)]">
+              <div className="relative size-[160px] overflow-hidden rounded-full bg-[rgba(0,0,0,0.001)] md:size-[240px] lg:size-[330px]">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                   src={item.imageSrc}
                   alt=""
-                  className="absolute left-1/2 top-1/2 h-[260px] -translate-x-1/2 -translate-y-1/2 object-cover"
+                  className="absolute bottom-0 left-1/2 h-4/5 -translate-x-1/2 object-cover"
                 />
                 <div className="pointer-events-none absolute inset-0 z-10 rounded-full shadow-[inset_6.07228px_4.55421px_13.6626px_rgba(237,204,232,0.65)]"></div>
               </div>
-              <div className="liquid text-subheader-1 absolute -bottom-10 left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-[40px] px-8 py-6 text-white backdrop-blur-md">
+              <div className="liquid text-subheader-1 absolute -bottom-6 left-1/2 z-10 -translate-x-1/2 whitespace-nowrap rounded-3xl px-6 py-3 text-white backdrop-blur-md md:-bottom-8 md:rounded-[30px] md:px-6 md:py-4 lg:-bottom-10 lg:rounded-[40px] lg:px-8 lg:py-6">
                 {item.title}
               </div>
             </div>
