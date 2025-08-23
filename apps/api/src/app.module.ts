01d54e5ff0e096108176a414d13854002d0c77d2
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2025-08-23 22:40:11 +0700

    add-auth-back

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { auth } from '@workspace/auth';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule.forRoot(auth), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
