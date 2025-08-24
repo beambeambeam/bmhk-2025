b37da22611abcd594f00e6f5a59ea3d57e376833
beambeambeam <supawitmarayat@gmail.com>
2025-08-24 14:30:01 +0700

    feat: enhance TeamRegisterForm with additional fields and select input for number of members

--- apps/web/app/(protected)/register/team/_components/form.tsx
+++ apps/web/app/(protected)/register/team/_components/form.tsx
@@ -5,6 +5,13 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "@workspace/ui/components/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@workspace/ui/components/form"
 import { Input } from "@workspace/ui/components/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select"
 import { useForm } from "react-hook-form"
 import z from "zod"
 
@@ -12,7 +19,7 @@ const teamRegisterSchema = z.object({
   team_image: z.instanceof(File).array().min(1, "จำเป็นต้องกรอกช่องนี้").max(1),
   team_name: z.string().min(1, "จำเป็นต้องกรอกช่องนี้").max(20, "ชื่อทีมต้องมีไม่เกิน 20 ตัวอักษร"),
   school_name: z.string().min(1, "จำเป็นต้องกรอกช่องนี้"),
-  quote: z.string().min(1, "จำเป็นต้องกรอกช่องนี้").max(50, "คำคมประจำทีมต้องมีไม่เกิน 50 ตัวอักษร"),
+  quote: z.string().max(50, "คำคมประจำทีมต้องมีไม่เกิน 50 ตัวอักษร"),
   number_of_member: z.number(),
 })
 
@@ -43,10 +50,59 @@ function TeamRegisterForm(props: FormProps<TeamRegisterSchemaType>) {
           name="team_name"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>team_name</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="team_name" {...field} />
               </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="school_name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>school_name</FormLabel>
+              <FormControl>
+                <Input placeholder="school_name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="quote"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>quote</FormLabel>
+              <FormControl>
+                <Input placeholder="quote" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="number_of_member"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>number_of_member</FormLabel>
+              <Select
+                onValueChange={(value) => field.onChange(Number(value))}
+                defaultValue={String(field.value)}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="2">2</SelectItem>
+                  <SelectItem value="3">3</SelectItem>
+                </SelectContent>
+              </Select>
               <FormMessage />
             </FormItem>
           )}
