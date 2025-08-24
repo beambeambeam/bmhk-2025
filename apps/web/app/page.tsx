fc6416c31a79d66e9f1cd85aaf1595893c9bd7c0
JAKKAPHAT CHALERMPHANAPHAN <gunt@guntxjakka.me>
2025-08-25 03:24:39 +0700

    perf: preload bg images

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,43 +9,71 @@ import { Navbar } from "@/app/_components/navbar"
 import QualificationSector from "@/app/_components/qualification"
 import Scope from "@/app/_components/scope"
 import Story from "@/app/_components/story"
+import Head from "next/head"
 
 import Footer from "./_components/footer"
 
 export default function Page() {
   return (
-    <div
-      className="no-scrollbar bg-home flex min-h-screen w-full snap-y snap-always flex-col gap-20 overflow-hidden overflow-y-hidden text-white"
-      style={{ scrollBehavior: "smooth" }}>
-      <Navbar />
+    <>
+      <Head>
+        <link
+          rel="preload"
+          as="image"
+          href="/static/background-image/Home.webp"
+          media="(min-width: 1024px)"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="/static/background-image/Home-tablet.webp"
+          media="(min-width: 768px)"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="/static/background-image/Home-phone.webp"
+          media="(max-width: 768px)"
+        />
+      </Head>
+      <div className="hidden">
+        <img src="/static/background-image/Home.webp" alt="" />
+        <img src="/static/background-image/Home-tablet.webp" alt="" />
+        <img src="/static/background-image/Home-phone.webp" alt="" />
+      </div>
+      <div
+        className="no-scrollbar bg-home flex min-h-screen w-full snap-y snap-always flex-col gap-20 overflow-hidden overflow-y-hidden text-white"
+        style={{ scrollBehavior: "smooth" }}>
+        <Navbar />
 
-      <div id="landing" className="flex min-h-screen items-center justify-center">
-        <LandingSection />
+        <div id="landing" className="flex min-h-screen items-center justify-center">
+          <LandingSection />
+        </div>
+        <div id="qualification" className="flex min-h-screen items-center justify-center">
+          <QualificationSector />
+        </div>
+        <div id="application" className="flex min-h-screen items-center justify-center">
+          <Application />
+        </div>
+        <div id="award" className="flex items-center justify-center">
+          <Award />
+        </div>
+        <div id="dateandcontest" className="flex items-center justify-center">
+          <DateAndContest />
+        </div>
+        <div id="scope" className="flex min-h-screen items-center justify-center">
+          <Scope />
+        </div>
+        <div id="contact" className="flex min-h-screen items-center justify-center">
+          <Contact />
+        </div>
+        <div id="story" className="flex min-h-screen items-center justify-center">
+          <Story />
+        </div>
+        <div className="flex items-center justify-center">
+          <Footer />
+        </div>
       </div>
-      <div id="qualification" className="flex min-h-screen items-center justify-center">
-        <QualificationSector />
-      </div>
-      <div id="application" className="flex min-h-screen items-center justify-center">
-        <Application />
-      </div>
-      <div id="award" className="flex items-center justify-center">
-        <Award />
-      </div>
-      <div id="dateandcontest" className="flex items-center justify-center">
-        <DateAndContest />
-      </div>
-      <div id="scope" className="flex min-h-screen items-center justify-center">
-        <Scope />
-      </div>
-      <div id="contact" className="flex min-h-screen items-center justify-center">
-        <Contact />
-      </div>
-      <div id="story" className="flex min-h-screen items-center justify-center">
-        <Story />
-      </div>
-      <div className="flex items-center justify-center">
-        <Footer />
-      </div>
-    </div>
+    </>
   )
 }
