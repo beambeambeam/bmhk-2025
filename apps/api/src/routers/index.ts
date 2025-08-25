f750fbd14a8f3666289169e16ca41db89d43069a
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-08-26 03:20:48 +0700

    release: pre landing release (#55)
    * chore: seperate staging environment (#50)
    
    * Feat/policy cookie consent (#52)
    
    * feat: add cookie consent
    
    * feat: add PolicyConsent component for cookie consent management
    
    * feat: allow CI to run on pull requests to staging branch
    
    * fix: replace breakpoint to accomodate landscape tablet (#53)
    
    * chore: add temporary subdomain for prod (#54)
    
    * Feat/add auth with postgres service (#56)
    
    * better-auth-next
    
    * fix db
    
    * feat: update env example and docker compose for dev
    
    * refactor: remove obsolete SQL schema and metadata files
    
    * feat: new dbs migrate
    
    * fix: update dotenv configuration to resolve environment file path
    
    * refactor: remove protected procedure from appRouter to simplify API structure
    
    * feat: add dotenv support and update environment variables in Next.js configuration
    
    * feat: create initial database schema with tables for account, advisor, file, member, session, teams, user, and verification
    
    * refactor: remove obsolete authentication route and update sign-in button to include callback URL
    
    * feat: add NEXT_PUBLIC_WEB_URL environment variable and update sign-in callback URL
    
    * feat: update sign-in page to use new callback URL and add NEXT_PUBLIC_WEB_URL to environment example
    
    * feat: add PostgreSQL service to production Docker Compose configuration with health checks and resource limits
    
    * feat: update environment variables for CORS and server URL configuration
    
    ---------
    
    Co-authored-by: Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: bbb <132804471+beambeambeam@users.noreply.github.com>
    Co-authored-by: Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>

--- apps/api/src/routers/index.ts
+++ apps/api/src/routers/index.ts
@@ -1,12 +1,18 @@
 import { publicProcedure } from "@/lib/orpc"
+<<<<<<< HEAD
 import { registerRouter } from "@/routers/register"
+=======
+>>>>>>> 9e6ac1d (release: pre landing release (#55))
 import type { RouterClient } from "@orpc/server"
 
 export const appRouter = {
   healthCheck: publicProcedure.handler(() => {
     return "OK"
   }),
+<<<<<<< HEAD
   register: registerRouter,
+=======
+>>>>>>> 9e6ac1d (release: pre landing release (#55))
 }
 export type AppRouter = typeof appRouter
 export type AppRouterClient = RouterClient<typeof appRouter>
