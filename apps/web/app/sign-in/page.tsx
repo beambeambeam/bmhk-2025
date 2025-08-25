ae845615f7a8b1b2ddbc0aa9bf53dc4052171e8c
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2025-08-25 22:52:00 +0700

    better-auth-next

--- apps/web/app/sign-in/page.tsx
+++ apps/web/app/sign-in/page.tsx
@@ -1,4 +1,16 @@
+"use client"
+
+import { authClient } from "@/lib/auth-client"
+
 function SignInPage() {
-  return <div>SignInPage</div>
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <button
+        onClick={() => authClient.signIn.social({ provider: "google" })}
+        className="cursor-pointer border-2">
+        sign in
+      </button>
+    </div>
+  )
 }
 export default SignInPage
