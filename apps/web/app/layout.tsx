46ef4a3d49c8480967f04ef179d1b9f7b9a144e6
Theeraphat Jaingam <jain.theera@gmail.com>
2025-08-26 22:54:58 +0700

    fix: smooth scrolling

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body className={cn("font-prompt antialiased", fontPrompt.variable, fontBaiJamjuree.variable)}>
         <Providers>
           {children}
