448c0112ab580476ba42c7ec61b05db7bb75ab46
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-08-27 09:58:32 +0700

    feat: add bg for register (#84)

--- apps/web/app/(protected)/register/(member)/layout.tsx
+++ apps/web/app/(protected)/register/(member)/layout.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@workspace/ui/lib/utils"
 import { ReactNode } from "react"
 
 interface MemberLayoutProps {
@@ -5,8 +6,11 @@ interface MemberLayoutProps {
 }
 
 function MemberLayout(props: MemberLayoutProps) {
+  const BACKGROUND_CLASS =
+    "bg-[url(/static/background-image/register-form/xs.webp)] md:bg-[url(/static/background-image/register-form/md.webp)] lg:bg-[url(/static/background-image/register-form/lg.webp)] 2xl:bg-[url(/static/background-image/register-form/2xl.webp)]"
+
   return (
-    <div className="flex h-full min-h-screen w-screen items-center justify-center bg-black">
+    <div className={cn("flex h-full min-h-screen w-screen items-center justify-center", BACKGROUND_CLASS)}>
       <div className="max-w-[62rem]">{props.children}</div>
     </div>
   )
