7eb704acccaee16b1a2673d0a84e89b1a769f943
Theeraphat Jaingam <jain.theera@gmail.com>
2025-08-28 02:32:06 +0700

    fix: minor

--- packages/ui/src/components/select.tsx
+++ packages/ui/src/components/select.tsx
@@ -50,7 +50,6 @@ function SelectContent({
         data-slot="select-content"
         className={cn(
           "border border-white/10 border-l-white/30 border-t-white/30 bg-white/15 backdrop-blur-md",
-          "aria-invalid:bg-[rgba(234,67,53,0.05)] aria-invalid:shadow-[inset_0px_1px_4px_rgba(234,67,53,0.6),inset_0px_-1px_4px_rgba(234,67,53,0.5)]",
           "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 max-h-(--radix-select-content-available-height) origin-(--radix-select-content-transform-origin) relative z-50 min-w-[8rem] overflow-y-auto overflow-x-hidden rounded-xl lg:rounded-2xl",
           position === "popper" &&
             "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
