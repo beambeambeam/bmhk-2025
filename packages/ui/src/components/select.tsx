53db64888041c696fc0d833d51f3b7dfbd979334
Theeraphat Jaingam <jain.theera@gmail.com>
2025-08-28 01:55:31 +0700

    fix: padding

--- packages/ui/src/components/select.tsx
+++ packages/ui/src/components/select.tsx
@@ -26,7 +26,7 @@ function SelectTrigger({
     <SelectPrimitive.Trigger
       data-slot="select-trigger"
       className={cn(
-        "text-body-3 aria-invalid:ring-error-500/20 aria-invalid:border-error-500 flex h-[38px] w-fit items-center justify-between gap-2 whitespace-nowrap rounded-xl border border-white/10 border-l-white/30 border-t-white/30 bg-white/15 px-3 py-2 outline-none transition-colors disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-gray-50 *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2 lg:h-[45px] lg:rounded-2xl 2xl:h-14 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-gray-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+        "text-body-3 aria-invalid:ring-error-500/20 aria-invalid:border-error-500 flex h-[38px] w-fit items-center justify-between gap-2 whitespace-nowrap rounded-xl border border-white/10 border-l-white/30 border-t-white/30 bg-white/15 px-3 py-1.5 outline-none transition-colors disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-gray-50 *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2 lg:h-[45px] lg:rounded-2xl 2xl:h-14 2xl:py-2.5 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-gray-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
         className
       )}
       {...props}>
