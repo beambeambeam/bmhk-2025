adee19540919096df17ecbd3687abc32beddc1a6
JAKKAPHAT CHALERMPHANAPHAN <gunt@guntxjakka.me>
2025-08-28 13:01:40 +0700

    fix: remove not null in advisor

---
+++ packages/db/drizzle/0007_perfect_gamma_corps.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "advisor" ALTER COLUMN "food_allergy" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "advisor" ALTER COLUMN "food_type" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "advisor" ALTER COLUMN "drug_allergy" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "advisor" ALTER COLUMN "chronic_disease" DROP NOT NULL;
