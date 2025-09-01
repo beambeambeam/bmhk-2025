9ec5cbdc75df179bc62d191064f1cb7ad0af0e0f
JAKKAPHAT CHALERMPHANAPHAN <gunt@guntxjakka.me>
2025-09-01 15:50:14 +0700

    build: add missing nextjs config to staffapp

--- apps/staff/next.config.ts
+++ apps/staff/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_STAFFAPP_URL: process.env.NEXT_PUBLIC_STAFFAPP_URL,
+  },
 }
 
 export default nextConfig
