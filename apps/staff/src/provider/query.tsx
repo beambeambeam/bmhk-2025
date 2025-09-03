9916ddd083ba4afd541340f666475560253b6180
beambeambeam <supawitmarayat@gmail.com>
2025-09-03 13:50:07 +0700

    feat: implement caching utilities and query provider for improved data management

---
+++ apps/staff/src/provider/query.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
+
+interface QueryProviderProps {
+  children: Readonly<ReactNode>
+}
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      gcTime: 1000 * 60 * 60 * 24,
+
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+
+      retry: 2,
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+
+      networkMode: "offlineFirst",
+    },
+    mutations: {
+      retry: 1,
+      networkMode: "offlineFirst",
+    },
+  },
+})
+
+function QueryProvider(props: QueryProviderProps) {
+  return <QueryClientProvider client={queryClient}>{props.children}</QueryClientProvider>
+}
+export default QueryProvider
