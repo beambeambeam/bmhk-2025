95a2a850512f7f9625fe7cd651bb50aa3b1d1a58
beambeambeam <supawitmarayat@gmail.com>
2025-09-03 17:34:30 +0700

    feat: base form

--- apps/staff/src/app/(protected)/round-1/_components/verifly-dialog/form.tsx
+++ apps/staff/src/app/(protected)/round-1/_components/verifly-dialog/form.tsx
@@ -1,17 +1,118 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { useState } from "react"
+import { Label } from "@/components/ui/label"
+import MultipleSelector from "@/components/ui/multiselect"
+import { Textarea } from "@/components/ui/textarea"
 
 type VerifyFormProps = {
   id: string
 }
 
 function VerifyForm(_props: VerifyFormProps) {
-  const [count, setCount] = useState(0)
   return (
-    <div>
-      <Button onClick={() => setCount((c) => c + 1)}>{count}</Button>
+    <div className="flex flex-col gap-4">
+      <Label>Adviser</Label>
+      <MultipleSelector
+        commandProps={{
+          label: "Select problems",
+        }}
+        placeholder="Select problems"
+        hideClearAllButton
+        hidePlaceholderWhenSelected
+        emptyIndicator={<p className="text-center text-sm">No problems found</p>}
+        options={[
+          {
+            label: "ข้อมูลไม่ตรง",
+            value: "data-mismatch",
+          },
+          // {
+          //   label: "ปพ.7 มีปัญหา",
+          //   value: "p7-problem",
+          // },
+          {
+            label: "บัตรประชาชนมีบัญหา",
+            value: "id-card-problem",
+          },
+          {
+            label: "บัตรอาจารย์มีบัญหา",
+            value: "teacher-card-problem",
+          },
+        ]}
+      />
+      <Label>Member 1</Label>
+      <MultipleSelector
+        commandProps={{
+          label: "Select problems",
+        }}
+        placeholder="Select problems"
+        hideClearAllButton
+        hidePlaceholderWhenSelected
+        emptyIndicator={<p className="text-center text-sm">No problems found</p>}
+        options={[
+          {
+            label: "ข้อมูลไม่ตรง",
+            value: "data-mismatch",
+          },
+          {
+            label: "ปพ.7 มีปัญหา",
+            value: "p7-problem",
+          },
+          {
+            label: "บัตรประชาชนมีบัญหา",
+            value: "id-card-problem",
+          },
+        ]}
+      />
+      <Label>Member 2</Label>
+      <MultipleSelector
+        commandProps={{
+          label: "Select problems",
+        }}
+        placeholder="Select problems"
+        hideClearAllButton
+        hidePlaceholderWhenSelected
+        emptyIndicator={<p className="text-center text-sm">No problems found</p>}
+        options={[
+          {
+            label: "ข้อมูลไม่ตรง",
+            value: "data-mismatch",
+          },
+          {
+            label: "ปพ.7 มีปัญหา",
+            value: "p7-problem",
+          },
+          {
+            label: "บัตรประชาชนมีบัญหา",
+            value: "id-card-problem",
+          },
+        ]}
+      />
+      <Label>Member 3</Label>
+      <MultipleSelector
+        commandProps={{
+          label: "Select problems",
+        }}
+        placeholder="Select problems"
+        hideClearAllButton
+        hidePlaceholderWhenSelected
+        emptyIndicator={<p className="text-center text-sm">No problems found</p>}
+        options={[
+          {
+            label: "ข้อมูลไม่ตรง",
+            value: "data-mismatch",
+          },
+          {
+            label: "ปพ.7 มีปัญหา",
+            value: "p7-problem",
+          },
+          {
+            label: "บัตรประชาชนมีบัญหา",
+            value: "id-card-problem",
+          },
+        ]}
+      />
+      <Label>หมายเหตุ</Label>
+      <Textarea className="h-50" />
     </div>
   )
 }
