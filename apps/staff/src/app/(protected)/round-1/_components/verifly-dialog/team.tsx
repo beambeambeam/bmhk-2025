777d4ac00a9d65006d97ea1db6703fe2d15f0bb0
beambeambeam <supawitmarayat@gmail.com>
2025-09-04 15:21:08 +0700

    refactor: simplify team image display in VeriflyDialog by updating image and no-image handling for improved layout consistency

--- apps/staff/src/app/(protected)/round-1/_components/verifly-dialog/team.tsx
+++ apps/staff/src/app/(protected)/round-1/_components/verifly-dialog/team.tsx
@@ -61,14 +61,9 @@ function TeamDisplay() {
             <Label>Team Image</Label>
             <div className="rounded-lg border p-2">
               {data.image ? (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  src={data.image.url}
-                  alt={data.image.name}
-                  className="h-[240px] w-full rounded object-contain md:h-[200px] lg:h-[180px] lg:w-[160px]"
-                />
+                <img src={data.image.url} alt={data.image.name} className="h-40 w-20" />
               ) : (
-                <div className="text-muted-foreground">No image</div>
+                <div className="text-muted-foreground h-40 w-20">No image</div>
               )}
             </div>
           </div>
