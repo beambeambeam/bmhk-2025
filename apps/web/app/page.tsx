f9037fee1d4fcada8d585ade9cccdf9cb312996b
beambeambeam <supawitmarayat@gmail.com>
2025-09-13 16:28:54 +0700

    feat: update landing page and navbar for improved user flow

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,14 +4,13 @@ import Application from "@/app/_components/application"
 import Award from "@/app/_components/award"
 import Contact from "@/app/_components/contact"
 import DateAndContest from "@/app/_components/dateandcontest"
+import Footer from "@/app/_components/footer"
 import LandingSection from "@/app/_components/landing"
 import { Navbar } from "@/app/_components/navbar"
 import QualificationSector from "@/app/_components/qualification"
 import Scope from "@/app/_components/scope"
 import Story from "@/app/_components/story"
 import Head from "next/head"
-
-import Footer from "./_components/footer"
 
 export default function Page() {
   return (
