31deafbaf2b38550c940fe8a7242fc0caa7032ef
beambeambeam <supawitmarayat@gmail.com>
2025-09-13 16:55:16 +0700

    style: update button border radius in TeamDone component for improved aesthetics

--- apps/web/app/(protected)/teams/done.tsx
+++ apps/web/app/(protected)/teams/done.tsx
@@ -254,7 +254,7 @@ function TeamDone() {
             {!isSubmit && (
               <Link
                 href="/register/team"
-                className="flex h-fit w-fit items-center justify-between gap-4 rounded-[24px] bg-[radial-gradient(105.85%_133.12%_at_50%_100%,#DFDFDF_0%,rgba(223,223,223,0)_100%)] px-6 py-3 md:w-auto md:px-8 2xl:px-10 2xl:py-4">
+                className="flex h-fit w-fit items-center justify-between gap-4 rounded-[32px] bg-[radial-gradient(105.85%_133.12%_at_50%_100%,#DFDFDF_0%,rgba(223,223,223,0)_100%)] px-6 py-3 md:w-auto md:px-8 2xl:px-10 2xl:py-4">
                 <span className="text-[1.125rem] font-medium md:text-[1.5rem] 2xl:text-[2rem]">
                   เลยกำหนดรับสมัคร
                 </span>
